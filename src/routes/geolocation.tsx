4f8a8cab815d8bc5fe2360859c35a6fc8c76732c
ZajoncM <milosz.zajonc@gmail.com>
2023-02-09 13:52:01 +0100

    fix geolocation

--- src/routes/geolocation.tsx
+++ src/routes/geolocation.tsx
@@ -1,59 +1,77 @@
 import { useEffect, useState } from "react";
 import Layout from "../layout/layout";
-import GoogleMapReact from "google-map-react";
-import { FaMapMarkerAlt } from "react-icons/fa";
-
-type MarkerProps = GoogleMapReact.Coords;
-
-const Marker = (_: MarkerProps) => <FaMapMarkerAlt size="1rem" color="red" />;
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 
 const Geolocation = () => {
-  const [center, setCenter] = useState<GoogleMapReact.Coords | undefined>(
-    undefined
-  );
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY || "",
+    libraries: ["places"],
+  });
+  const [center, setCenter] = useState<google.maps.LatLngLiteral | null>();
+  const [found, setFound] = useState<{
+    location: google.maps.LatLngLiteral;
+    name: string;
+  } | null>();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(({ coords }) => {
       setCenter({ lat: coords.latitude, lng: coords.longitude });
     });
-
-    const fetchNearestRestaurant = async () => {
-      const url =
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
-        "location=" +
-        center?.lat +
-        "," +
-        center?.lng +
-        "&radius=100&type=restaurant&key=<My API Key>";
-
-      fetch(
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=100&type=restaurant&key=AIzaSyCZUMvOWqr5bfJceXgt37pFf-twnYLg-8Y"
-      )
-        .then((response) => response.json())
-        .then((JsonResponse) => {
-          console.log(JsonResponse);
-        })
-        .catch((error) => {
-          alert(error);
-        });
-    };
-
-    fetchNearestRestaurant();
   }, []);
 
-  if (!center) return <div>loading...</div>;
+  const onLoad = (map: google.maps.Map) => {
+    let request: google.maps.places.FindPlaceFromQueryRequest = {
+      query: "restaurant",
+      fields: ["name", "geometry"],
+      locationBias: { ...center! },
+    };
+
+    const service = new google.maps.places.PlacesService(map);
+    service.findPlaceFromQuery(request, (results) => {
+      if (results && results[0].geometry?.location && results[0].name) {
+        console.log(results[0]);
+        setFound({
+          location: {
+            lat: results[0].geometry?.location.lat(),
+            lng: results[0].geometry?.location.lng(),
+          },
+          name: results[0].name,
+        });
+      }
+    });
+  };
+
+  if (!isLoaded) return <div>Loading...</div>;
 
   return (
     <Layout>
-      <div style={{ width: "100%", flexGrow: "1" }}>
-        <GoogleMapReact
-          defaultCenter={center}
-          defaultZoom={15}
-          key="AIzaSyCZUMvOWqr5bfJceXgt37pFf-twnYLg-8Y"
+      {center && (
+        <GoogleMap
+          mapContainerStyle={{
+            width: "100%",
+            height: "100%",
+          }}
+          zoom={17}
+          center={center}
+          onLoad={onLoad}
         >
-          <Marker lat={center.lat} lng={center.lng} />
-        </GoogleMapReact>
-      </div>
+          <Marker position={center} />
+          {found && (
+            <Marker
+              position={found.location}
+              label={found.name}
+              icon={{
+                path: "M12.75 0l-2.25 2.25 2.25 2.25-5.25 6h-5.25l4.125 4.125-6.375 8.452v0.923h0.923l8.452-6.375 4.125 4.125v-5.25l6-5.25 2.25 2.25 2.25-2.25-11.25-11.25zM10.5 12.75l-1.5-1.5 5.25-5.25 1.5 1.5-5.25 5.25z",
+                fillColor: "#34495e",
+                fillOpacity: 1,
+                strokeColor: "#000",
+                strokeWeight: 1,
+                scale: 1,
+              }}
+            />
+          )}
+        </GoogleMap>
+      )}
     </Layout>
   );
 };
