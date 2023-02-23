4779e2e18cd11be978b7ea52eaec2d793b882580
ZajoncM <milosz.zajonc@gmail.com>
2023-02-23 15:48:25 +0100

    fix device orientation

--- src/routes/device-position.tsx
+++ src/routes/device-position.tsx
@@ -1,26 +1,52 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Layout from "../layout/layout";
 
 const DevicePosition = () => {
   const [deviceOrientation, setDeviceOrientation] =
     useState<DeviceOrientationEvent | null>(null);
+  let windowListenerExist = false;
+
+  const getPermission = () => {
+    if (!("DeviceOrientationEvent" in window)) {
+      console.log("DeviceOrientationEvent not supported");
+      supWarn.current.style.display = "inline";
+      return Promise.resolve("denied");
+    }
+    console.log("DeviceOrientationEvent exist");
+
+    return (
+      DeviceOrientationEvent?.requestPermission?.() ||
+      Promise.resolve("granted")
+    );
+  };
+
+  const handleOrientation = (event: DeviceOrientationEvent) => {
+    setDeviceOrientation(event);
+  };
+
+  const handleStart = () => {
+    getPermission()
+      .then((positionAccess) => {
+        if (positionAccess === "granted") {
+          window.addEventListener("deviceorientation", handleOrientation, true);
+          windowListenerExist = true;
+        }
+      })
+      .catch((error) => console.log(error));
+  };
 
   useEffect(() => {
-    if (typeof window.DeviceOrientationEvent === "function") {
-      window.addEventListener(
-        "deviceorientation",
-        (e) => {
-          setDeviceOrientation(e);
-        },
-        false
-      );
-    } else {
-      console.log("orientation not supported");
-    }
+    return () => {
+      if (windowListenerExist)
+        window.removeEventListener("deviceorientation", handleOrientation);
+    };
   }, []);
 
   return (
     <Layout>
+      <button className="clClick" onClick={handleStart}>
+        start
+      </button>
       <p style={{ fontSize: "40px" }}>
         beta: {deviceOrientation?.beta?.toFixed(2) || 0}
       </p>
